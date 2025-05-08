24ac87a8fdf47b6f8c87d74db7da87482b940e93
Usmaan <muhammad.usman02@arbisoft.com>
2025-05-08 16:30:07 +0500

    Added organization check while login

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -2,15 +2,29 @@ from django.contrib.auth import login
 from knox.views import LoginView as KnoxLoginView
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.exceptions import ValidationError
+from api.organizations.models import Organization
 
 
 class LoginView(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
-        # TODO: Check user login based on `organization`
+        organization_slug = request.data.get('organization_slug')
+        if not organization_slug:
+            raise ValidationError({'organization_slug': 'This field is required.'})
+
+        try:
+            organization = Organization.objects.get(slug=organization_slug, is_active=True)
+        except Organization.DoesNotExist:
+            raise ValidationError({'authorization': 'Organization does not exist.'})
+
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
+
+        if user.organization != organization:
+            raise ValidationError({'authorization': 'Unable to log in with provided credentials.'})
+
         login(request, user)
         return super().post(request, format=None)
